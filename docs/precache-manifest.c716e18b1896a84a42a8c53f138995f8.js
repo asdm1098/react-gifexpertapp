858efd76eef4c777ae311a48fa158795bf59accd
Stiven Diaz <Stivendiaz98@gmail.com>
2020-07-29 23:09:23 -0500

    docs agregado

---
+++ docs/precache-manifest.c716e18b1896a84a42a8c53f138995f8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a3dd2e6753e0fbbed5fa7b1fffd9c64",
+    "url": "/index.html"
+  },
+  {
+    "revision": "78a25c5f85ca7598bc9b",
+    "url": "/static/css/main.c45cfea2.chunk.css"
+  },
+  {
+    "revision": "85a4a86f3fe06f17fcc7",
+    "url": "/static/js/2.8249eaeb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8249eaeb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "78a25c5f85ca7598bc9b",
+    "url": "/static/js/main.8b4ed119.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
